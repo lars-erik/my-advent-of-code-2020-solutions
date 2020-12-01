f59f236b56c5e4656a4b422b56549ae81a7177a7
Lars-Erik Aabech <lea@markedspartner.no>
2020-12-02 00:43:09 +0100

    Pretty much all methods explored with C#

--- 01/csharp/Calculating_Expense_Bills.cs
+++ 01/csharp/Calculating_Expense_Bills.cs
@@ -1,6 +1,7 @@
 using System;
 using System.IO;
 using System.Linq;
+using System.Linq.Expressions;
 using NUnit.Framework;
 
 namespace day1
@@ -12,7 +13,7 @@ namespace day1
         [SetUp]
         public void Initialize_Input()
         {
-            var inputStream = GetType()
+            using var inputStream = GetType()
                 .Assembly
                 .GetManifestResourceStream("day1.input.txt");
             input = new StreamReader(inputStream)
@@ -26,7 +27,7 @@ namespace day1
         public void Need_Product_Of_2020_Pairs()
         {
             var output = input
-                .Join(input, x => true, x => true, (x, y) => (x, y))
+                .Join(input, x => true, y => true, (x, y) => (x, y))
                 .Where(xy => xy.x + xy.y == 2020)
                 .Select(xy => xy.x * xy.y)
                 .First();
@@ -35,14 +36,62 @@ namespace day1
         }
         
         [Test]
-        public void Need_Product_Of_2020_Triplets()
+        public void Need_Product_Of_2020_Join()
         {
             var output = input
-                .Join(input, x => true, x => true, (x, y) => (x, y))
-                .Join(input, x => true, x => true, (xy, z) => new[]{xy.x, xy.y, z})
-                .Where(xyz => xyz.Sum() == 2020)
-                .Select(xyz => xyz.Aggregate((p, n) => p * n))
+                .Join(input, x => true, y => true, (x, y) => (x, y))
+                .Join(input, x => true, y => true, (xy, z) => (xy.x, xy.y, z))
+                .Where(xyz => xyz.x + xyz.y + xyz.z == 2020)
+                .Select(xyz => xyz.x * xyz.y * xyz.z)
                 .First();
+            
+            Assert.That(output, Is.EqualTo(244300320));
+        }
+        
+        [Test]
+        public void Need_Product_Of_2020_SelectMany()
+        {
+            var output = input.SelectMany(
+                    x => input.SelectMany(
+                    y => input.Select(
+                    z => (x, y, z)
+                )))
+                .Where(xyz => xyz.x + xyz.y + xyz.z == 2020)
+                .Select(xyz => xyz.x * xyz.y * xyz.z)
+                .First();
+            
+            Assert.That(output, Is.EqualTo(244300320));
+        }
+        
+        [Test]
+        public void Need_Product_Of_2020_LINQ()
+        {
+            var output = (
+                from x in input
+                from y in input
+                from z in input
+                where
+                    x + y + z == 2020
+                select
+                    x * y * z
+                )
+                .First();
+
+            Assert.That(output, Is.EqualTo(244300320));
+        }
+
+        [Test]
+        public void Need_Product_Of_2020_Triplets_Loop()
+        {
+            Func<int> findTriplet = () => {
+                foreach(var x in input)
+                    foreach(var y in input)
+                        foreach(var z in input)
+                            if (x + y + z == 2020)
+                                return x * y * z;
+                return -1;
+            };
+            var output = findTriplet();
             
             Assert.That(output, Is.EqualTo(244300320));
         }
