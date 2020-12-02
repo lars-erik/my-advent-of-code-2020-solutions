db3f9f5c1a797a618e99046aa71b9d1c6c3c5397
Lars-Erik Aabech <lea@markedspartner.no>
2020-12-02 14:04:50 +0100

    Day 2 done

--- 02/csharp/Validating_Passwords.cs
+++ 02/csharp/Validating_Passwords.cs
@@ -13,7 +13,7 @@ namespace day2 {
         [TestCase("2-9 c: ccccccccc", true)]
         public void Enforces_Policy(string input, bool expectedValidity)
         {
-            bool isValid = Validate(input);
+            bool isValid = Validate(input, SledRentalPolicy);
             Assert.That(isValid, Is.EqualTo(expectedValidity));
         }
 
@@ -32,15 +32,36 @@ namespace day2 {
         }
 
         [Test]
-        public void Counts_Valid_Passwords()
+        public void Counts_Valid_SledRental_Passwords()
         {
             var valid = input
-                .Select(Validate)
+                .Select(ValidateSledRentalPolicy)
                 .Count(x => x);
+
             Assert.That(valid, Is.EqualTo(524));
         }
 
-        public static bool Validate(string input)
+        [Test]
+        public void Counts_Valid_ToboganCorporate_Passwords()
+        {
+            var valid = input
+                .Select(ValidateToboganCorporatePolicy)
+                .Count(x => x);
+
+            Assert.That(valid, Is.EqualTo(485));
+        }
+
+        private static bool ValidateSledRentalPolicy(string record)
+        {
+            return Validate(record, SledRentalPolicy);
+        }
+
+        private static bool ValidateToboganCorporatePolicy(string record)
+        {
+            return Validate(record, ToboganCorporatePolicy);
+        }
+
+        public static bool Validate(string input, Func<(int[] range, char chr), string, bool> policy)
         {
             var set = input.Split(':').Select(x => x.Trim()).ToArray();
             var ruleSet = set[0].Split(' ');
@@ -48,13 +69,19 @@ namespace day2 {
                 range: ruleSet[0].Split('-').Select(x => Convert.ToInt32(x)).ToArray(),
                 chr: ruleSet[1][0]
             );
-            bool isValid = SledRentalPolicy(rule, set[1]);
+            bool isValid = policy(rule, set[1]);
             return isValid;
         }
 
+        private static bool ToboganCorporatePolicy((int[] range, char chr) rule, string password)
+        {
+            var isValid = (password[rule.range[0] - 1] == rule.chr) 
+                        ^ (password[rule.range[1] - 1] == rule.chr);
+            return isValid;
+        }
         private static bool SledRentalPolicy((int[] range, char chr) rule, string password)
         {
-            var chars = password.Where(x => x == rule.chr).Count();
+            var chars = password.Count(x => x == rule.chr);
             var isValid = chars >= rule.range[0] && chars <= rule.range[1];
             return isValid;
         }
